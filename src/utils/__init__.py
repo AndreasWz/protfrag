bd49fe14f9875df94eeedcffb7134232a1ff573d
AndreasWz <andreas.weitz@mailbox.org>
2025-11-04 14:16:28 +0100

    feat(src): fix __init__.py's

---
+++ src/utils/__init__.py
@@ -0,0 +1,8 @@
+# src/utils/__init__.py
+
+# Make the FragmentAnnotationParser available at the package level
+from .fragment_parser import FragmentAnnotationParser
+
+__all__ = [
+    "FragmentAnnotationParser"
+]
